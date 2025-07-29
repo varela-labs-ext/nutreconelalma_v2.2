2a93e2853bc7f6fc8e19e1168109b732a654bffe
Joseph Varela <varela.badilla@gmail.com>
2025-07-28 22:44:30 -0600

    cleaning

--- src/context/ComparisonContext/ComparisonProvider.tsx
+++ src/context/ComparisonContext/ComparisonProvider.tsx
@@ -3,42 +3,22 @@ import { getClassName, Logger } from '@/utils/logger';
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import useMixingCenterContext from '../MixingCenterContext/useMixingCenterContext';
 import ComputerBigGroupModel from '@/logic/models/ComputerBigGroupModel';
-import { getAdditionalCostsSummary, getProductionPerMonth, updateAdditionalCostsSummary } from '../MixingCenterContext/MixingCenterUtils';
+import { getProductionPerMonth } from '../MixingCenterContext/MixingCenterUtils';
 import AdditionalCostsTotalsModel from '@/logic/models/AdditionalCostsTotalsModel';
 import MixingCenterOperatingResourcesModel from '@/logic/models/MixingCenterOperatingResourcesModel';
-import { deepClone } from '@/utils/objectUtils';
 import MixingCenterSettingsModel from '@/logic/models/common/MixingCenterSettingsModel';
 import MixingCenterRawMaterialsModel from '@/logic/models/MixingCenterRawMaterialsModel';
 import { BuildTextPlainReport, calculateResources, calculateResourcesTotal } from './ComparisonProviderUtils';
-import { padText } from '@/components/business/reporting/textPadding';
-import ResultItemModel from '@/logic/models/ResultItemModel';
-import JustValueItemModel from '@/logic/models/row_item/OneValueItemRowModel';
-
-
-// type MixingCenterComparisonState = {
-//     manual: MixingCenterResultsModel | null;
-//     automated: MixingCenterResultsModel | null;
-// };
 
 type ComparisonContextType = {
     startComparision: boolean;
     results: MixingCenterResultsModel;
     printingResults: string[];
-    // loadResults: (inData: MixingCenterResultsModel) => void;
     resetResults: () => void;
     setStartComparision: (value: boolean) => void;
 };
 
-// const initialState: MixingCenterComparisonState = {
-//     manual: null,
-//     automated: null,
-// };
-
 export const ComparisonContext = createContext<ComparisonContextType | undefined>(undefined);
-
-// type Props = {
-//     children: React.ReactNode;
-// };
 
 export const ComparisonProvider = ({ children }: { children: React.ReactNode }) => {
     const { buildBackupPayload } = useMixingCenterContext();
@@ -228,7 +208,6 @@ export const ComparisonProvider = ({ children }: { children: React.ReactNode }) 
 
             const brandNewResults: MixingCenterResultsModel = new MixingCenterResultsModel();
 
-
             const dataBackupPayload: ComputerBigGroupModel = buildBackupPayload();
 
             const summaryManual: AdditionalCostsTotalsModel = calculateResources(dataBackupPayload.backup_MC_Manual_Resources);
@@ -238,61 +217,19 @@ export const ComparisonProvider = ({ children }: { children: React.ReactNode }) 
             calculateResourcesTotal(summaryAutomatic, false);
 
             if (dataBackupPayload.mixingCenterSettings !== null) {
-                // const settings: MixingCenterSettingsModel = dataBackupPayload.mixingCenterSettings;
-
                 setGeneralSettings(brandNewResults, dataBackupPayload.mixingCenterSettings);
-
-                // brandNewResults.lineasProduccion.value = settings.productionLines;
-                // brandNewResults.produccionDiaria.value = settings.productionPerDay;
-                // brandNewResults.produccionMensual.value = getProductionPerMonth(settings);
-                // brandNewResults.porcentajeAdulto.value = settings.percentPerAdult;
-                // brandNewResults.porcentajePediatric.value = settings.percentPerPediatric;
-                // brandNewResults.porcentajeNeonatal.value = settings.percentPerNeonatal;
             }
 
             setTotalResourcesManual(brandNewResults, summaryManual);
 
-            // brandNewResults.PersonalProtectiveMaterialsCosts.valueNptManual = summaryManual.protectiveMaterialsTotal;
-            // brandNewResults.hygieneNCleaningMaterialsCosts.valueNptManual = summaryManual.hygieneNCleanlinessTotal;
-            // brandNewResults.maintenanceCosts.valueNptManual = summaryManual.maintenanceTotal;
-            // brandNewResults.productionCosts.valueNptManual = summaryManual.productionTotal;
-            // brandNewResults.sterileEquipmentCosts.valueNptManual = summaryManual.sterilizedEquipmentTotal;
-            // brandNewResults.automatedEquipmentCosts.valueNptManual = summaryManual.automatedEquipmentTotal;
-
             setTotalResourcesAutomatic(brandNewResults, summaryAutomatic);
 
-
-            // brandNewResults.PersonalProtectiveMaterialsCosts.valueNptAutomatic = summaryAutomatic.protectiveMaterialsTotal;
-            // brandNewResults.hygieneNCleaningMaterialsCosts.valueNptAutomatic = summaryAutomatic.hygieneNCleanlinessTotal;
-            // brandNewResults.maintenanceCosts.valueNptAutomatic = summaryAutomatic.maintenanceTotal;
-            // brandNewResults.productionCosts.valueNptAutomatic = summaryAutomatic.productionTotal;
-            // brandNewResults.sterileEquipmentCosts.valueNptAutomatic = summaryAutomatic.sterilizedEquipmentTotal;
-            // brandNewResults.automatedEquipmentCosts.valueNptAutomatic = summaryAutomatic.automatedEquipmentTotal;
-
             if (dataBackupPayload.backup_MC_Manual_Resources !== null) {
-                // const resourcesManual: MixingCenterOperatingResourcesModel = dataBackupPayload.backup_MC_Manual_Resources;
-
                 setTotalStaffManual(brandNewResults, dataBackupPayload.backup_MC_Manual_Resources);
-
-                // brandNewResults.chemicalStaffHours.valueNptManual = resourcesManual.staffChemistSalary.horasPersonalFarmaceuticoPorNP.value;
-                // brandNewResults.costPerChemicalStaff.valueNptManual = resourcesManual.staffChemistSalary.costoPersonalFarmaceuticoPorPreparacion.value;
-
-                // brandNewResults.auxiliaryStaffHours.valueNptManual = resourcesManual.staffAssistantSalary.horasPersonalFarmaceuticoPorNP.value;
-                // brandNewResults.costPerAuxiliaryStaff.valueNptManual = resourcesManual.staffAssistantSalary.costoPersonalFarmaceuticoPorPreparacion.value;
-                // brandNewResults.automatedEquipmentCosts.valueNptManual = 0;
             }
 
             if (dataBackupPayload.backup_MC_Automatic_Resources !== null) {
-                // const resourcesAutomatic: MixingCenterOperatingResourcesModel = dataBackupPayload.backup_MC_Automatic_Resources;
-
                 setTotalStaffAutomatic(brandNewResults, dataBackupPayload.backup_MC_Automatic_Resources);
-
-                // brandNewResults.chemicalStaffHours.valueNptAutomatic = resourcesAutomatic.staffChemistSalary.horasPersonalFarmaceuticoPorNP.value;
-                // brandNewResults.costPerChemicalStaff.valueNptAutomatic = resourcesAutomatic.staffChemistSalary.costoPersonalFarmaceuticoPorPreparacion.value;
-
-                // brandNewResults.auxiliaryStaffHours.valueNptAutomatic = resourcesAutomatic.staffAssistantSalary.horasPersonalFarmaceuticoPorNP.value;
-                // brandNewResults.costPerAuxiliaryStaff.valueNptAutomatic = resourcesAutomatic.staffAssistantSalary.costoPersonalFarmaceuticoPorPreparacion.value;
-
             }
 
             if (dataBackupPayload.backup_MC_Manual_RawMaterials !== null) {
@@ -306,62 +243,7 @@ export const ComparisonProvider = ({ children }: { children: React.ReactNode }) 
                 setTotals1NptPediatricManual(brandNewResults, rawMaterials, operationResourcesAndStaff);
                 setTotals1NptNeonatalManual(brandNewResults, rawMaterials, operationResourcesAndStaff);
 
-                // if (brandNewResults.porcentajeAdulto.value > 0) {
-                //     // Costos 1 NPT adulto
-                //     brandNewResults.cost1NptAdult.valueNptManual =
-                //         rawMaterials.adultoRawMaterial.total *
-                //         rawMaterials.adultoRawMaterial.cantidad;
-
-                //     brandNewResults.costoTotalPreparacionNptAdult.valueNptManual =
-                //         brandNewResults.cost1NptAdult.valueNptManual +
-                //         operationResourcesAndStaff;
-                // } else {
-                //     brandNewResults.cost1NptAdult.valueNptManual = 0;
-                //     brandNewResults.costoTotalPreparacionNptAdult.valueNptManual = 0;
-                // }
-
-                // if (brandNewResults.porcentajePediatric.value > 0) {
-                //     brandNewResults.cost1NptPediatric.valueNptManual =
-                //         rawMaterials.pediatricoRawMaterial.total *
-                //         rawMaterials.pediatricoRawMaterial.cantidad;
-
-                //     brandNewResults.costoTotalPreparacionNptPediatric.valueNptManual =
-                //         brandNewResults.cost1NptPediatric.valueNptManual +
-                //         operationResourcesAndStaff;
-                // } else {
-                //     brandNewResults.cost1NptPediatric.valueNptManual = 0;
-                //     brandNewResults.costoTotalPreparacionNptPediatric.valueNptManual = 0;
-                // }
-
-                // if (brandNewResults.porcentajeNeonatal.value > 0) {
-                //     brandNewResults.cost1NptNeonatal.valueNptManual =
-                //         rawMaterials.neonatalRawMaterial.total *
-                //         rawMaterials.neonatalRawMaterial.cantidad;
-
-                //     brandNewResults.costoTotalPreparacionNptNeonatal.valueNptManual =
-                //         brandNewResults.cost1NptNeonatal.valueNptManual +
-                //         operationResourcesAndStaff;
-                // } else {
-                //     brandNewResults.cost1NptNeonatal.valueNptManual = 0;
-                //     brandNewResults.costoTotalPreparacionNptNeonatal.valueNptManual = 0;
-                // }
-
                 setTotalsByPopulationManual(brandNewResults, rawMaterials);
-
-                // brandNewResults.valorTotalAdult.valueNptManual = (
-                //     brandNewResults.produccionDiaria.value *
-                //     (brandNewResults.porcentajeAdulto.value / 100)) *
-                //     rawMaterials.adultoRawMaterial.total;
-
-                // brandNewResults.valorTotalPediatric.valueNptManual = (
-                //     brandNewResults.produccionDiaria.value *
-                //     (brandNewResults.porcentajePediatric.value / 100)) *
-                //     rawMaterials.pediatricoRawMaterial.total;
-
-                // brandNewResults.valorTotalNeonatal.valueNptManual = (
-                //     brandNewResults.produccionDiaria.value *
-                //     (brandNewResults.porcentajeNeonatal.value / 100)) *
-                //     rawMaterials.neonatalRawMaterial.total;
 
                 brandNewResults.valorTotalNutriciosDia.valueNptManual =
                     brandNewResults.valorTotalAdult.valueNptManual +
@@ -377,50 +259,8 @@ export const ComparisonProvider = ({ children }: { children: React.ReactNode }) 
                     brandNewResults.costPerAuxiliaryStaff.valueNptAutomatic;
 
                 setTotals1NptAdultAutomatic(brandNewResults, rawMaterialsAutomatic, operationResourcesAndStaff);
-
-                // brandNewResults.cost1NptAdult.valueNptAutomatic =
-                //     rawMaterialsAutomatic.adultoRawMaterial.total *
-                //     rawMaterialsAutomatic.adultoRawMaterial.cantidad;
-
-                // brandNewResults.costoTotalPreparacionNptAdult.valueNptAutomatic =
-                //     brandNewResults.cost1NptAdult.valueNptAutomatic +
-                //     operationResourcesAndStaff;
-
                 setTotals1NptPediatricAutomatic(brandNewResults, rawMaterialsAutomatic, operationResourcesAndStaff);
-
-                // brandNewResults.cost1NptPediatric.valueNptAutomatic =
-                //     rawMaterialsAutomatic.pediatricoRawMaterial.total *
-                //     rawMaterialsAutomatic.pediatricoRawMaterial.cantidad;
-
-                // brandNewResults.costoTotalPreparacionNptPediatric.valueNptAutomatic =
-                //     brandNewResults.cost1NptPediatric.valueNptAutomatic +
-                //     operationResourcesAndStaff;
-
                 setTotals1NptNeonatalAutomatic(brandNewResults, rawMaterialsAutomatic, operationResourcesAndStaff);
-
-                // brandNewResults.cost1NptNeonatal.valueNptAutomatic =
-                //     rawMaterialsAutomatic.neonatalRawMaterial.total *
-                //     rawMaterialsAutomatic.neonatalRawMaterial.cantidad;
-
-                // brandNewResults.costoTotalPreparacionNptNeonatal.valueNptAutomatic =
-                //     brandNewResults.cost1NptNeonatal.valueNptAutomatic +
-                //     operationResourcesAndStaff;
-
-
-                // brandNewResults.valorTotalAdult.valueNptAutomatic = (
-                //     brandNewResults.produccionDiaria.value *
-                //     (brandNewResults.porcentajeAdulto.value / 100)) *
-                //     rawMaterialsAutomatic.adultoRawMaterial.total;
-
-                // brandNewResults.valorTotalPediatric.valueNptAutomatic = (
-                //     brandNewResults.produccionDiaria.value *
-                //     (brandNewResults.porcentajePediatric.value / 100)) *
-                //     rawMaterialsAutomatic.pediatricoRawMaterial.total;
-
-                // brandNewResults.valorTotalNeonatal.valueNptAutomatic = (
-                //     brandNewResults.produccionDiaria.value *
-                //     (brandNewResults.porcentajeNeonatal.value / 100)) *
-                //     rawMaterialsAutomatic.neonatalRawMaterial.total;
 
                 setTotalsByPopulationAutomatic(brandNewResults, rawMaterialsAutomatic);
 
--- src/logic/defaults/RawMaterialAdultDefaults.ts
+++ src/logic/defaults/RawMaterialAdultDefaults.ts
@@ -40,41 +40,11 @@ class RawMaterialAdultDefaults extends DefaultsRawMaterialsBase<RawMaterialGroup
 
     protected setCommons_MixingCentral_Automatic(inItem: RawMaterialGroupModel): void {
         this.setCommons_MixingCentral_Manual(inItem);
-
-        // this.setClinicalInput(inItem.AguaEsteril_500ml, 500, 100, 1.0, 0, 4000.00, 0); //4000.00
-        // this.setClinicalInput(inItem.CloruroPotasioVial_10cc, 10.00, 30.00, 1.00, 0, 1500.00, 0); //1500.00
-        // this.setClinicalInput(inItem.CloruroSodioVial_10cc, 10.00, 30.00, 0, 0, 1500.00, 0); //1500.00
-        // this.setClinicalInput(inItem.Complejo_B, 0, 1.00, 0, 0, 3000.00, 0); //0.00
-        // this.setClinicalInput(inItem.Dextrosa_50p_Bolsa_500cc, 500.00, 250.00, 0, 0, 10814.00, 0); //10814.00
-        // this.setClinicalInput(inItem.FosfatoPotasioVial_10cc, 10.00, 10.00, 1.00, 0, 5700.00, 0); //5700.00
-        // this.setClinicalInput(inItem.GluconatoCalcioVial_10cc, 10.00, 20.00, 0, 0, 1800.00, 0); //1800.00
-        // this.setClinicalInput(inItem.Multivitaminas, 0, 0, 0, 0, 25000.00, 0); //0.00
-        // this.setClinicalInput(inItem.SulfatoMagnesioVial_10cc, 10.00, 12.00, 1.00, 0, 1500.00, 0); //1500.00
-        // this.setClinicalInput(inItem.Vitamina_C, 0, 1.00, 0, 0, 2000.00, 0); //0.00
-        // this.setClinicalInput(inItem.VitaminasHidrosolubles, 0, 1.00, 0, 0, 23000.00, 0); //23000.00
     }
 
     protected setOthers_MixingCentral_Automatic(inItem: RawMaterialGroupModel): void {
         this.setOthers_MixingCentral_Manual(inItem);
-
-        // this.setClinicalInput(inItem.Aminoacidos15_1000Ml, 1000.00, 0, 0, 0, 135000.00, 0); // 0.00
-        // this.setClinicalInput(inItem.Aminoacidos15_500Ml, 500.00, 0, 0, 0, 66000.00, 0); // 0.00
-        // this.setClinicalInput(inItem.AminoacidosConElectrolitos10_500Ml, 500.00, 0, 0, 0, 61000.00, 0); // 0.00
-        // this.setClinicalInput(inItem.AminoacidosSinElectrolitos10_500Ml, 500.00, 600.00, 1.00, 0, 40000.00, 0); //40000.00
-
-        // this.setClinicalInput(inItem.ElementosTraza10Ml, 10.00, 0, 1.00, 0, 20000.00, 0); //20000.00
-        // this.setClinicalInput(inItem.GlicerofosfatoSodio20Ml, 20.00, 0, 1.00, 0, 20000.00, 0); //20000.00
-        // this.setClinicalInput(inItem.LipidosLipofundin500Cc, 500.00, 200.00, 0, 0, 80000.00, 0); //80000.00
-
-        // this.setClinicalInput(inItem.MixingEva1000Ml, 0, 0, 0, 0, 31000.00, 0); // 0.00
-        // this.setClinicalInput(inItem.MixingEva2000Ml, 0, 1.00, 1.00, 0, 39000.00, 0); //39000.00
-        // this.setClinicalInput(inItem.MixingEva250Ml, 1000.00, 0, 0, 0, 31000.00, 0); // 0.00
-        // this.setClinicalInput(inItem.MixingEva500Ml, 500.00, 0, 0, 0, 30000.00, 0); // 0.00
-
-        // this.setClinicalInput(inItem.Tiamina, 0, 1.00, 0, 0, 3000.00, 0); // 0.00
-        // this.setClinicalInput(inItem.VitaminasLiposolublesAdulto, 0, 1.00, 1.00, 0, 13000.00, 0); //13000.00
     }
-
 }
 
 export default RawMaterialAdultDefaults;
